eafb2eb0b87b05bc22c3f868948ec51fcbdce2a0
meerjan <janushaik9191@gmail.com>
2025-02-25 23:25:45 +0530

    ec2-demo

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,17 +1,17 @@
 terraform {
-    required_providers{
-        aws = {
-            source = "hashicorp/aws"
-            version = "5.88.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.88.0"
     }
-backend "s3"{
-    bucket = "daws-s3-remote"
-    key = "ec2-demo"
-    region = "us-east-1"
+  }
+  backend "s3" {
+    bucket         = "daws-s3-remote"
+    key            = "ec2-demo"
+    region         = "us-east-1"
     dynamodb_table = "daws-dynamodb"
+  }
 }
-}
-provider "aws"{
-    region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
